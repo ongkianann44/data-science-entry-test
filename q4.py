632e80ba8db338f9ed742669b99391feffedc458
ongkianann44 <ongkianann44@gmail.com>
2026-06-02 18:34:48 +0800

    Update q4.py

--- q4.py
+++ q4.py
@@ -5,10 +5,24 @@ def string_reverse(s):
     - s must be a string.
     - Return the reversed string.
     """
-    return
+    # Validate that 's'
+    if not isinstance(s, str):
+        print("Error: input must be a string.")
+        return s
+    
+    rev_str = ""
+    for char in s:
+        rev_str = char + rev_str
+    
+
+    return rev_str
 
 
 # Task 2
 # Invoke the function "string_reverse" using the following scenarios:
 # - "Hello World"
 # - "Python"
+
+#print(string_reverse("Hello World"))
+#print(string_reverse(123))
+print(string_reverse("Python"))
